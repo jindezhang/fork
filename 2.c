f635be2eb27c41012b79530482cc011465361925
jindezhang <1028611544@qq.com>
2017-07-31 15:22:35 +0800

    parent and chlid process are Concurrence

--- 2.c
+++ 2.c
@@ -5,15 +5,15 @@ int main(int argc, char const *argv[])
 {
 	int i;
 	pid_t a;
-	for(i = 0; i<10 ;i++)
+	for(i = 0; i<50;i++)
 	{
+		sleep(2);
 		printf("i =%d uid is %d\n",i,getpid() );
 		a = fork();
 		if(a > 0)
 			break;
 	}
 	if(a >0)
-	// 	for(i = 0; i<10 ;i++)
-			wait(NULL);
+		wait(NULL);
 	return 0;
 }
